932cbed503035c2da4ddb8d660c5183cc4d2dc8d
GitAvi001 <avindusridharmawardhana@gmail.com>
2025-06-08 21:09:53 +0530

    Configured circuit breaker dependencies and BookingService Route filters

--- apigateway/src/main/java/com/example/apigateway/route/BookingServiceRoutes.java
+++ apigateway/src/main/java/com/example/apigateway/route/BookingServiceRoutes.java
@@ -1,13 +1,17 @@
 package com.example.apigateway.route;
 
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
 
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 
@@ -17,9 +21,19 @@ public class BookingServiceRoutes {
 @Bean
     public RouterFunction<ServerResponse> BoookingRoutes(){
         return GatewayRouterFunctions.route("booking-service")
-              .route(RequestPredicates.POST("/api/v1/booking"),
+                .route(RequestPredicates.POST("/api/v1/booking"),
                       HandlerFunctions.http("http://localhost:8081/api/v1/booking"))
-              .build();
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("bookingServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
+                .build();
 }
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute() {
+        return GatewayRouterFunctions.route("fallbackRoute")
+                .POST("/fallbackRoute",
+                        request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                                .body("Booking service is down"))
+                .build();
+    }
 
 }
